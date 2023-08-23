4946fa5e41ee3481b67950a8201e2d9c61f4fa00
Ramazan Dönmez <ramazan.dnmz@gmail.com>
2023-08-23 16:59:21 +0300

    t

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,10 +1,22 @@
 const hre = require("hardhat");
+const { Wallet } = require('ethers');
+const { utils } = require('ethers');
+const ethers = require('ethers')
 
-async function main() {
-    const wallet = hre.ethers.Wallet.createRandom();
+async function main() { 
+
+    const wallet = Wallet.fromMnemonic(utils.entropyToMnemonic(utils.randomBytes(32)) )
+    //let wallet = hre.ethers.Wallet.createRandom([ethers.utils.randomBytes(32)]);
+ 
     console.info("Address:", wallet.address);
     console.info("Private Key:", wallet.privateKey);
-    console.info("Mnemonic:", wallet.mnemonic.phrase);
+    console.info("Mnemonic:", wallet.mnemonic.phrase); 
+}
+
+async function getRandomNumber() {
+    return Math.floor(
+        Math.random() * (9999999 - 1) + 1
+      );
 }
 
 main().catch((err)=> {
