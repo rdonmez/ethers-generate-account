52e1716fd6b6cc3ea0a820747a83a99bfba544f2
Ramazan Dönmez <ramazan.dnmz@gmail.com>
2024-01-16 14:28:52 +0300

    changes

--- scripts/generate.js
+++ scripts/generate.js
@@ -5,18 +5,31 @@ const ethers = require('ethers')
 
 async function main() { 
 
-    const wallet = Wallet.fromMnemonic(utils.entropyToMnemonic(utils.randomBytes(32)) )
+    //const wallet = Wallet.fromMnemonic(utils.entropyToMnemonic(utils.randomBytes(32)) )
     //let wallet = hre.ethers.Wallet.createRandom([ethers.utils.randomBytes(32)]);
  
-    console.info("Address:", wallet.address);
-    console.info("Private Key:", wallet.privateKey);
-    console.info("Mnemonic:", wallet.mnemonic.phrase); 
+    //console.info("Address:", wallet.address);
+    //console.info("Private Key:", wallet.privateKey);
+    //console.info("Mnemonic:", wallet.mnemonic.phrase); 
+
+    await getFromMnemonic();
 }
 
 async function getRandomNumber() {
     return Math.floor(
         Math.random() * (9999999 - 1) + 1
       );
+}
+
+async function getFromMnemonic() {
+   
+    const wallet = Wallet.fromMnemonic(
+        utils.entropyToMnemonic(utils.randomBytes(32))
+      )
+      
+      console.log('wallet.address:', wallet.address)
+      console.log('wallet.mnemonic.phrase:', wallet.mnemonic.phrase)
+      console.log('wallet.privateKey:', wallet.privateKey)
 }
 
 main().catch((err)=> {
